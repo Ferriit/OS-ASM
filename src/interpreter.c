b68dff10117fce80c7b890d519e82f48406817f8
Ferriit <ferriitalt@gmail.com>
2026-03-01 23:47:41 +0100

    fixed bugs in tokenizer

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,10 +1,3 @@
-#include "../headers/mem.h"
-#include "../headers/dynamic_array.h"
-#include "../headers/hashmap.h"
-#include "../headers/io.h"
-#include "../headers/utils.h"
-
-
 #include "interpreter.h"
 
 typedef enum {
